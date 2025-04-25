0772fc0c4c69199d824b0ac967416e51229363c0
lanceshields69 <lookthing@gmail.com>
2025-04-24 21:56:35 -0700

    undid the google analytics addition

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@vercel/analytics/react"
 import { Suspense } from "react"
-import { GoogleAnalytics } from "@next/third-parties/google"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -59,12 +59,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-H8JW9EH3HY" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H8JW9EH3HY');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
           <Suspense>{children}</Suspense>
         </ThemeProvider>
         <Analytics />
-        <GoogleAnalytics gaId="G-H8JW9EH3HY" />
       </body>
     </html>
   )
