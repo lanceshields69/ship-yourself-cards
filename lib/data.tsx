4446fc19954488db9f9e79107841055d54ea1a1b
lanceshields69 <lookthing@gmail.com>
2025-04-27 18:53:31 -0700

    Add files via upload

--- lib/data.tsx
+++ lib/data.tsx
@@ -121,8 +121,8 @@ export const categories: Category[] = [
           "Protect your peak energy hours for creation.",
           "End each day by planning the next.",
         ],
-        quote: "The worst enemy to creativity is self-doubt.",
-        quoteAuthor: "Sylvia Plath",
+        quote: "Either you run the day or the day runs you.",
+        quoteAuthor: "Jim Rohn",
       },
       {
         id: "mindset-6",
